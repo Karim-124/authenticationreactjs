af66c44dfeeb04630addddf70d7c1e10a452408a
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-03-03 02:16:00 +0200

    make the field required.

--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -14,16 +14,20 @@ function SiginUp() {
                     <h2 className="text-center text-3xl font-bold mb-4">Sign Up</h2>
 
                     <div className="mb-4">
+                        <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Name</label>
+                        <input type="text" id="name" class="rounded bg-white border border-sky-300 block w-full text-sm p-2.5" placeholder="Enter your Name" required />
+                    </div>
+                    <div className="mb-4">
                         <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
-                        <input type="text" id="email" class="rounded bg-white border border-sky-300 block w-full text-sm p-2.5" placeholder="Enter your email" />
+                        <input type="email" id="email" class="rounded bg-white border border-sky-300 block w-full text-sm p-2.5" placeholder="Enter your email" required />
                     </div>
                     <div className="mb-4">
                         <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                        <input type="password" id="password" class="rounded bg-white border border-sky-300 block w-full text-sm p-2.5" placeholder="Enter your password" />
+                        <input type="password" id="password" class="rounded bg-white border border-sky-300 block w-full text-sm p-2.5" placeholder="Enter your password"  required/>
                     </div>
                     <div className="mb-6">
                         <label htmlFor="confirm-password" className="block text-gray-700 text-sm font-bold mb-2">Confirm Password</label>
-                        <input type="password" id="confirm-password" class="rounded bg-white border border-sky-300 	 block w-full text-sm p-2.5" placeholder="Confirm your password" />
+                        <input type="password" id="confirm-password" class="rounded bg-white border border-sky-300 	 block w-full text-sm p-2.5" placeholder="Confirm your password"  required/>
                     </div>
                     <button type="submit" className="w-full mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2.5 px-4 rounded">Sign Up</button>
                     <p className=' text-center mt-4  text-1xl'>Already have an account? <Link className='underline underline-offset-4 text-blue-600' to='/login'>Log In</Link> </p>
