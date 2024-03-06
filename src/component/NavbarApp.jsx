32621d6487d0c3b184556bf78b56076a16f1c40d
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-03-07 01:42:33 +0200

    add padding to navbar.

--- src/component/NavbarApp.jsx
+++ src/component/NavbarApp.jsx
@@ -9,7 +9,7 @@ function NavbarApp() {
     };
 
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <nav className="bg-white border-gray-200 dark:bg-gray-900 p-2">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
@@ -32,7 +32,7 @@ function NavbarApp() {
                         <li>
                             <Link to="/signup" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign Up</Link>
                         </li>
-                       
+
                     </ul>
                 </div>
             </div>
