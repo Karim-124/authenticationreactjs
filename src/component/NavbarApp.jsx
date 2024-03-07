a7ee0f618ad89bcb814524c3b72289dfc0592701
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-03-07 03:20:04 +0200

    remove padding from navbar and edit the topBar.

--- src/component/NavbarApp.jsx
+++ src/component/NavbarApp.jsx
@@ -8,14 +8,13 @@ function NavbarApp() {
         setIsOpen(!isOpen);
     };
 
-    function handleLogout()
-    {
+    function handleLogout() {
         window.localStorage.removeItem("email");
         window.location.pathname = "/";
     }
 
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900 p-2">
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link
                     to="/"
@@ -85,15 +84,15 @@ function NavbarApp() {
                             </Link>
                         </li>
                     </ul>
-                </div> : <li>
-                        <Link
-                        onClick={handleLogout}    
+                </div> :
+                    <Link
+                        onClick={handleLogout}
                         to="/signup"
-                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                        class="block  px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     >
                         Logout
                     </Link>
-                </li>}
+                }
             </div>
         </nav>
     );
