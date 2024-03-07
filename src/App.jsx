47f6a178af57fa26cc9ccadaa8eae891e02123fe
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-03-07 02:00:10 +0200

    add routes for to go to hompage componet.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import SiginUp from './component/SignUp';
 import Login from './component/Login';
+import Hompage from './component/Hompage';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path='/' element={<Hompage />} />
         <Route path='/signup' element={<SiginUp />} />
         <Route path='/login' element={<Login />} />
       </Routes>
