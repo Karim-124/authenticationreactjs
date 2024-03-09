98e498d2341479301512b2827c04e18555139c74
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-03-09 13:53:38 +0200

    fetch the data of users to show it.

--- src/component/Users.jsx
+++ src/component/Users.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/user/show")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+  }, [])
+  const showData = users.map((user) => {
+    return (
+      <>
+        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <th
+            scope="row"
+            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+          >
+            Apple MacBook Pro 17"
+          </th>
+          <td class="px-6 py-4">Silver</td>
+          <td class="px-6 py-4">Laptop</td>
+          <td class="px-6 py-4">$2999</td>
+        </tr></>
+    )
+  })
   return (
     <>
       <div class="relative overflow-x-auto">
@@ -22,39 +44,8 @@ function Users() {
             </tr>
           </thead>
           <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Apple MacBook Pro 17"
-              </th>
-              <td class="px-6 py-4">Silver</td>
-              <td class="px-6 py-4">Laptop</td>
-              <td class="px-6 py-4">$2999</td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Microsoft Surface Pro
-              </th>
-              <td class="px-6 py-4">White</td>
-              <td class="px-6 py-4">Laptop PC</td>
-              <td class="px-6 py-4">$1999</td>
-            </tr>
-            <tr class="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td class="px-6 py-4">Black</td>
-              <td class="px-6 py-4">Accessories</td>
-              <td class="px-6 py-4">$99</td>
-            </tr>
+        
+           {showData} 
           </tbody>
         </table>
       </div>
