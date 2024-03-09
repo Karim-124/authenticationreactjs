98b748e8c39599dfe0d3a38bc159d98fb0ab571c
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-03-09 15:10:34 +0200

    apply function delete users form the site .

--- src/component/Users.jsx
+++ src/component/Users.jsx
@@ -1,48 +1,66 @@
 import React, { useEffect, useState } from "react";
-
+import "../component/all.min.css";
+import axios from "axios";
 function Users() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/user/show")
       .then((res) => res.json())
-      .then((data) => setUsers(data))
-  }, [])
+      .then((data) => setUsers(data));
+  }, [users]);
+
+  function deleteUser(id) {
+    axios.delete(`http://127.0.0.1:8000/api/user/delete/${id}`);
+    console.log("data");
+  }
   const showData = users.map((user, index) => {
     return (
       <>
-        <tr key={index} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+        <tr
+          key={index}
+          className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+        >
           <th
             scope="row"
-            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center"
           >
             {index}
           </th>
-          <td class="px-6 py-4">{user.name}</td>
-          <td class="px-6 py-4">{user.email}</td>
+          <td className="px-6 py-4 text-center">{user.name}</td>
+          <td className="px-6 py-4 text-center">{user.email}</td>
+          <td className="px-6 py-4 text-center">
+            <i
+              onClick={() => deleteUser(user.id)}
+              class="fa-solid fa-trash text-2xl text-red-400 cursor-pointer"
+            ></i>
+            <i className="fa-solid fa-pen-to-square text-2xl ml-2 hover:text-sky-400 cursor-pointer"></i>
+          </td>
+        </tr>
+      </>
+    );
+  });
 
-        </tr></>
-    )
-  })
   return (
     <>
-      <div class="relative overflow-x-auto">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-left  text-gray-500">
+          <thead className="bg-gray-500 text-white">
             <tr>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3 text-center">
                 Users id
-              </th><th scope="col" class="px-6 py-3">
+              </th>
+              <th scope="col" className="px-6 py-3 text-center">
                 Users Name
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3 text-center">
                 Email
+              </th>
+              <th scope="col" className="px-6 py-3 text-center">
+                Update & Delete
               </th>
             </tr>
           </thead>
-          <tbody>
-
-            {showData}
-          </tbody>
+          <tbody>{showData}</tbody>
         </table>
       </div>
     </>
